22423ec949869b20cd5dc2923fce82e818411485
Samantha Crespo <samkcrespo@gmail.com>
2022-04-07 09:56:53 -0700

    commit tree ab1202ba17d8e78c99a3b8260bb6f278ebbaae7d

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(record) {
+    let answer = record.find(function (game) {
+    return game.result === 'W'
+    });
+if (answer) {
+return answer.year;
+}
+
+}
+
+
+
+
